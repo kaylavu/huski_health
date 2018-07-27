18d9c53a3df905ef5aa8caf67cb6d6926b68e273
kaylavu <kaylaavu@gmail.com>
2018-07-27 18:20:24 +0000

    commit to server/database

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -13,6 +14,17 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      </div>
+      
+      <div className="card">
+      <img className="card-img-top" src="http://s2.thingpic.com/images/QN/tqh1UmemP1KTQFkvjkoSyMWL.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">My Cat</h5>
+        <p className="card-text">This Cat is fat.</p>
+        <p className="card-weight"> 200 lbs. </p>
+        <a href="pets" className="btn btn-primary">Pet Details</a>
+      </div>
+      </div>
       </div>
     );
   }
